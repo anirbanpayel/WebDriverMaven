e14b3418e68148256048f4a59903e880add99ad5
anirbanpayel <anirbanchattopadhyay18@gmail.com>
2016-11-15 00:40:39 -0500

    first code change

--- src/test/java/Running_FirstTest.java
+++ src/test/java/Running_FirstTest.java
@@ -1,18 +1,32 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
 
 public class Running_FirstTest {
 	
 	
+	public static WebDriver driver;
+	public Running_FirstTest()
+	{
+	System.setProperty("webdriver.ie.driver", "C:\\Users\\anirbanpayel\\Desktop\\IEDriverServer.exe");
+	 driver= new InternetExplorerDriver();
+	}
+	
 	@Test
 	
 	public void launchUrl()
 	{
-		System.setProperty("webdriver.ie.driver", "C:\\Users\\anirbanpayel\\Desktop\\IEDriverServer.exe");
-		WebDriver driver= new InternetExplorerDriver();
+		
 		driver.get("http://www.google.com");
+	}
+	
+	@AfterSuite
+	
+	public void teardown()
+	{
+		driver.quit();
 	}
 
 }
